28c1a4d6c39744916e1351a94970f08f59f82456
Davi <48662592+d-magit@users.noreply.github.com>
2021-09-29 05:45:53 -0300

    Add files via upload

---
+++ Scripts/confmaker.py
@@ -0,0 +1,15 @@
+import json
+
+maindomain = "media.lolisociety.com"
+movies = []
+
+with open(r'../Results/noimglinks.json', 'r', encoding="utf8") as f:
+    movies = json.loads(f.read())
+
+with open(r'../Results/movies.conf', 'a', encoding='utf8') as f:
+    for i in movies:
+        f.write(f"location /movies/{i[7]}/" + " {\n    proxy_pass " + f"{i[6][:i[6].rfind('/')+1]};" + "\n}\n\n")
+        i[6] = f"https://{maindomain}/movies/{i[7]}{i[6][i[6].rfind('/'):]}"
+
+with open(r'../Results/linkspoofed.json', 'w') as f:
+    f.write(json.dumps(movies))
